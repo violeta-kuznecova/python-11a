1317b168106769ed05af2041e9a57696e77aff95
LikelyFox <86975955+LikelyFox@users.noreply.github.com>
2024-10-28 12:54:09 +0200

    Create uzd1.py

---
+++ uzd1.py
@@ -0,0 +1,19 @@
+try:
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+except ValueError:
+print('x or y = int')
+x1 = (2 + x) / (x * y)
+x2 = 5*x*3 - x*2 + 7*x - 6
+
+x4 = (2**(x*y)) / (5*y)
+x3 = (x*y ** 0.5)
+print(x1)
+print(x2)
+
+print(x3)
+print(x4)
+
+
+
+
