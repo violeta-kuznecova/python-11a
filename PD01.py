50f522e7d387554bb301af03b5fe48314e577e69
violeta-kuznecova <violetakuznevowa8@gmail.com>
2024-11-04 12:48:48 +0200

    Add files via upload

---
+++ PD01.py
@@ -0,0 +1,47 @@
+x=float(input(10))
+y=float(input(5))
+
+print("15:",10+5)
+print("5:",10-5)
+print("50:",10*5)
+print("2:",10/5)
+
+
+number=float(input("10:"))
+
+if number > 0:
+    print("10.")
+elif number < 0:
+    print("-10.")
+else:
+    print("0.")
+    
+    
+grade = int(input("1-10:"))
+
+if 9 <= grade <=10:
+    print("A:")
+elif 7 <= grade <=8:
+    print("B:")
+elif 5 <= grade <=6:
+ print("C:")
+elif 3 <= grade <=4:
+    print("D:")
+elif 1 <= grade <=2:
+    print("E:")
+elif grade == 0:
+    print("F:")
+else:
+    print((0-10))
+    
+    
+     
+    
+    
+
+    
+    
+
+
+
+
